94c187f56239bd017aded78604eef8f3dcd402d6
Thanusree <thanusreepula@gmail.com>
2026-02-22 11:27:42 +0530

    Fix UC2: remove old UC1 code, keep only ASCII banner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,19 +1,19 @@
 /**
  * OOPSBannerApp
+ * UC2 - OOPS Banner Display Application
+ * Displays OOPS in ASCII banner using spaces and *
+ * 
  * @author Thanusree
  * @version 2.0
  */
-
 public class OOPSBannerApp {
     public static void main(String[] args) {
-
         System.out.println(" *****    *****   *****    ***** ");
-        System.out.println("*     *  *     *  *     * *     ");
-        System.out.println("*     *  *     *  *     * *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
         System.out.println("*     *  *****    *****    *****  ");
         System.out.println("*     *  *        *             *      ");
         System.out.println("*     *  *        *             *      ");
         System.out.println(" *****   *        *        *****  ");
-
     }
 }
