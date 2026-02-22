0653679c2420d632f41881e552a1a3882819b51c
Thanusree <thanusreepula@gmail.com>
2026-02-22 10:05:22 +0530

    UC1: Print literal OOPS to console

---
+++ src/OOPSBannerApp.java
@@ -0,0 +1,13 @@
+/**
+ * OOPSBannerApp UC1 - OOPS Banner Display Application
+ * This class demonstarates a simple java application that displays the object 
+ * oriented programming system OOPS acronym to the console.
+ * 
+ * @author Thanusree
+ * @version 1.0
+ */
+ public class OOPSBannerApp {
+	public static void main(String[] args){
+	    System.out.println("OOPS");
+	}
+}
