c56e6067079dda0fc0098a5f263bd5b36360826b
Thanusree <thanusreepula@gmail.com>
2026-02-22 11:51:39 +0530

    Add UC2 code

---
+++ UC2/OOPSBannerAppUC2.java
@@ -0,0 +1,19 @@
+/**
+ * OOPSBannerApp
+ * UC2 - OOPS Banner Display Application
+ * Displays OOPS in ASCII banner using spaces and *
+ * 
+ * @author Thanusree
+ * @version 2.0
+ */
+public class OOPSBannerAppUC2 {
+    public static void main(String[] args) {
+        System.out.println(" *****    *****   *****    ***** ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *     *  *     *  *     ");
+        System.out.println("*     *  *****    *****    *****  ");
+        System.out.println("*     *  *        *             *      ");
+        System.out.println("*     *  *        *             *      ");
+        System.out.println(" *****   *        *        *****  ");
+    }
+}
