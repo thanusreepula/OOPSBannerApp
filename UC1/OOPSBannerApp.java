8e00049af49a3b2c1726357dcbaefa14a600bc1a
Thanusree <thanusreepula@gmail.com>
2026-02-22 11:51:27 +0530

    Add UC1 code

---
+++ UC1/OOPSBannerApp.java
@@ -0,0 +1,14 @@
+/**
+ * OOPSBannerApp
+ * UC1 - OOPS Banner Display Application
+ * 
+ * 
+ * @author Thanusree
+ * @version 2.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+        
+    }
+}
