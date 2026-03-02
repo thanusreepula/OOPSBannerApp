f7aa95efaec5a69eccc855ce09097d9734cd09b1
Thanusree <thanusreepula@gmail.com>
2026-03-02 12:51:18 +0530

    UC3 - Refactored banner using String.join()

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,19 +1,26 @@
-/**
- * OOPSBannerApp
- * @author Thanusree
- * @version 2.0
- */
-
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****    *****   *****    ***** ");
-        System.out.println("*     *  *     *  *     * *     ");
-        System.out.println("*     *  *     *  *     * *     ");
-        System.out.println("*     *  *****    *****    *****  ");
-        System.out.println("*     *  *        *             *      ");
-        System.out.println("*     *  *        *             *      ");
-        System.out.println(" *****   *        *        *****  ");
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", " ***** ", " ***** "));
 
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*      "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*     *", "*      "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", " ***** ", " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*      ", "      *"));
+
+        System.out.println(String.join(" ",
+                "*     *", "*     *", "*      ", "      *"));
+
+        System.out.println(String.join(" ",
+                " ***** ", " ***** ", "*      ", " ***** "));
     }
 }
